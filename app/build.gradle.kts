a00513d6c622131a7ff893f0e929fd16e8bd56fa
Aunali321 <48486084+Aunali321@users.noreply.github.com>
2023-05-24 01:28:32 +0530

    fix: failing build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.10.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
 
-    implementation("androidx.activity:activity-compose:1.8.0-alpha04")
+    implementation("androidx.activity:activity-compose:1.5.1")
 
     val composeVersion = "1.4.0-alpha03"
     implementation("androidx.compose.ui:ui:${composeVersion}")
